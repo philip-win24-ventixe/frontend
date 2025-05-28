5268500cb59e24dd45a3eb7ea1bec82579e94103
Philip Lindskog <philip_lindskog@hotmail.com>
2025-05-28 13:02:54 +0200

    Fixade med bild samt småfix på event-card

--- src/assets/components/EventItem.jsx
+++ src/assets/components/EventItem.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const EventItem = ({item}) => {
+
+
   return (
     <Link to={`/events/${item.id}`}>
       <div className='event-card'>
         <div className='image-box'></div>
-        <span className='event-card-date'>{item.eventDate}</span>
+        <span className='event-card-date'>{new Date(item.eventDate).toLocaleDateString()} - {new Date(item.eventDate).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit'})}</span>
         <span className='event-card-title'>{item.title}</span>
         <span className='event-card-location'>
           <i className="fa-regular fa-location-dot event-card-pin"></i>
